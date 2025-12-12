653b89590e41187a48b7e8c5b5eff086e9aaf7c6
abdcantcode <abdullah72ftw@gmail.com>
2025-12-13 02:47:39 +0530

    prototype

--- JS THE ADVANCE 1/script.js
+++ JS THE ADVANCE 1/script.js
@@ -16,7 +16,7 @@ let remote1=new Remote
 
 class Baigan{
   constructor(product,price){
-    this.product=product;
+    this.product=product; //idhar this ki value naya banra so object rehta
     this.price=price;
   }
   open(){
@@ -28,4 +28,12 @@ class Baigan{
 }
 let baigan1=new Baigan("DELL",60);
 
-// Class objects ke liye blueprint h, new se object banta aur constructor se woh object ke values set hunge, class ka naam humesha capital rakho 
+// Class objects ke liye blueprint h, new se object banta aur constructor se woh object ke values set hunge, class ka naam humesha capital rakho, Constructor automatically chalne waala function hai jab ek class ka ek instance create hota jab 
+//Agar class ke prototype mein kuch bhi add kardiye toh uske har ek instance mein share hojata woh
+//Constructor ke baher kuch bhi daalo woh prototype mein add hojayega... idhar jaisa open() aur close() huwa....
+Baigan.prototype.kholo=function(){
+  console.log("Baigan");
+}
+Baigan.prototype.bgnvariable=12;
+//Yeh do har ek usmein share hota basicallly....
+
