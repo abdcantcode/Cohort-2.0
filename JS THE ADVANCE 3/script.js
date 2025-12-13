0b24a1c966f05d3c29322165ad158713046d604a
abdcantcode <abdullah72ftw@gmail.com>
2025-12-13 19:26:44 +0530

    Inheritance

--- JS THE ADVANCE 3/script.js
+++ JS THE ADVANCE 3/script.js
@@ -7,3 +7,26 @@ let apniclass=class{
 console.log(apniclass.name);
 
 //Isku hoisting work nai karti... Agar console.log() karey first line mein toh work nai karta is what I mean
+
+//Inheritance
+class animal{
+  constructor(){
+    this.legs=2;
+    this.hands=2;
+  }
+  eat(){};
+  sleep(){};
+}
+
+//Yeh hai inheritance toh ismein octopus mein legs aur hands rehte plus eat aur sleep bhi rehta
+class octopus extends animal{
+  constructor(){
+    super();
+    this.hands=4;
+    this.legs=4;
+  }
+  munch(){
+
+  };
+}
+//So octopus has munch(), eat() and sleep() as well
