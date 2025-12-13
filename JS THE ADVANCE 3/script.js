1f821070944921d97e25ead533491fbeedf75c5f
abdcantcode <abdullah72ftw@gmail.com>
2025-12-13 19:21:06 +0530

    Hoisting

---
+++ JS THE ADVANCE 3/script.js
@@ -0,0 +1,9 @@
+//Class expression bolto basically variable ke through class banana
+let apniclass=class{
+  constructor(){
+    this.name="Abdullah";
+  }
+}
+console.log(apniclass.name);
+
+//Isku hoisting work nai karti... Agar console.log() karey first line mein toh work nai karta is what I mean
