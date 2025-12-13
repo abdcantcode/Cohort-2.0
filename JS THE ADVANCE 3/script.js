18ca80d1b497d3c16ca5e2110b60677d01fac686
abdcantcode <abdullah72ftw@gmail.com>
2025-12-13 19:37:56 +0530

    GETTERS AND SETTERS

--- JS THE ADVANCE 3/script.js
+++ JS THE ADVANCE 3/script.js
@@ -30,3 +30,25 @@ class octopus extends animal{
   };
 }
 //So octopus has munch(), eat() and sleep() as well
+  
+//Getter and setter ka concept
+class baalu{
+  constructor(){
+    this._age=12;
+    //generally _ ke saath variable rakhe toh people this its meant to be private but it can be accessed obviously...
+  }
+  set age(val){//Can be named anything basically age set karne use hota iney, iney ek param leta 
+    if(val<0){
+      console.log("Pgl hai kya re");
+      return;
+    }
+    this._age=val;
+    return this._age;
+  }
+  get age(){
+    console.log(this._age);
+  }
+}
+let nayavar=new baalu();
+nayavar.age=12;//Set karne 
+console.log(nayavar);//Get karne 
