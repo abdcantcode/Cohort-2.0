ca58175ca722e61a0bbdf5640ba713182a857ff3
abdcantcode <abdullah72ftw@gmail.com>
2025-12-15 04:29:38 +0530

    Section 2

--- JS THE ADVANCE Day 5/script.js
+++ JS THE ADVANCE Day 5/script.js
@@ -25,3 +25,52 @@ function afterDelay(time,cb){
 afterDelay(3000,function(){
   console.log("Callback executed");
 })
+
+
+// ## Exercise 2 — Intermediate (Data flow)
+
+// **Task (Hindi):** Ek function banao `getUser`
+
+// **Requirements:**
+// - `getUser` `username` lega
+// - 1 second ke baad `callback` ko ek object de:
+//   - `id`
+//   - `username`
+
+// **Then:**
+// - Callback ke andar ek aur function call karo `getUserPosts`
+
+// **`getUserPosts` requirements:**
+// - `userId` lega
+// - 1 second ke baad `callback` ko `posts` ka array de
+
+// **Final output:**
+// - User ka `username` print ho
+// - Fir uske `posts` print ho
+
+// **Goal:**
+// - Samajhna ki ek async ka result next async ko kaise milta hai
+// - Callback chaining practice
+
+// ---
+
+
+function getUser(username, cb) {
+  setTimeout(() => {
+    cb({ id: 1000, username: "_itz_abd" });
+  }, 1000);
+}
+
+function getUserPosts(id, cb) {
+  setTimeout(() => {
+    cb(["Post1", "Post2"]);
+  }, 1000);
+}
+
+getUser("Abdullah", function (data) {
+  console.log(user.username)
+  getUserPosts(data.id, function (allposts) {
+    console.log(allposts);
+  });
+});
+
