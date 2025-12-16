cd92ba7af6575db02db83841978ebbb77a4869c6
abdcantcode <abdullah72ftw@gmail.com>
2025-12-16 23:07:21 +0530

    Async await se solve kardiye apan alhamdulillah

--- JS THE ADVANCE Day 7/script.js
+++ JS THE ADVANCE Day 7/script.js
@@ -22,15 +22,40 @@ Build a small weather dashboard that fetches current weather data from a public 
 - Show user-friendly error messages for network errors, invalid input, or API errors.
 - Demonstrate at least one custom thrown error (e.g., `ExtremeTemperatureError`) and handle it in the UI. */
 
-function getWeather(city) {
-    let apikey = 'd6d96dd96d736553e1660d99053e99b2';
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apikey}`)
-      .then(function (rawdata) {
-        return rawdata.json();
-      })
-      .then(function (data) {
-        console.log(data.main.temp);
-        alert(data.main.temp+"°C");
-      })
+// function getWeather(city) {
+//     let apikey = 'd6d96dd96d736553e1660d99053e99b2';
+//     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apikey}`)
+//       .then(function (rawdata) {
+//         return rawdata.json();
+//       })
+//       .then(function (data) {
+//         console.log(data.main.temp);
+//         alert(data.main.temp+"°C");
+//       })
+// }
+// getWeather("Hyderabad");
+
+async function weatherphek(city){
+  try{
+    let apikey = '2772fb0b5e2f3c047a6c8414a62696e9';
+    let rawdata=await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apikey}`);
+    if(!rawdata.ok){
+      throw new Error("Something is wrong maybe the city is wrong"); //Yeh jaata catch block mein
+    }
+    let realdata=await rawdata.json(); //uppar ke await ke liye iney toh wait karna ich hai na obviously
+    console.log(realdata.main.temp+"C");
+    if(realdata.main.temp<0){
+      console.warn("Bhai thanda hai baher nakko nikal");
+    }
+    else if (realdata.main.temp>40){
+      console.warn("Bhai garmi mein pagal hojata re haule");
+    }
+    else{
+      console.log("Baher mausam bhi suhana hai mama chalo");
+    }
+  }
+  catch(err){
+    console.log(err.message);
+  }
 }
-getWeather("Hyderabad");
+weatherphek("Hyderabad");
