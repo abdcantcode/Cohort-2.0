d41a135e5eb9f61d6326f7027a8ea70eba56dca0
abdcantcode <abdullah72ftw@gmail.com>
2025-12-18 09:58:38 +0530

    spread op

---
+++ JS THE ADVANCE Day 8/script.js
@@ -0,0 +1,8 @@
+//arr1=[1,2,4];
+//arr2=arr1 se reference copy hoti exact cheez copy nai hoti 
+
+arr1=[1,2,3];
+arr2=[...arr1];
+arr3=[arr1[0],arr1[1],arr1[2]];//iske badle spread use karle sakte
+//ino hai spread operator 
+let a=arr1[0];
